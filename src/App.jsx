c7d3f3afe626e98bf8ceb304df7923400c781b85
Cristi <cristian.capota@ltniorga.ro>
2024-12-08 16:18:05 +0200

    fix deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
 import Hero from "./components/hero"
 import Prices from './components/prices'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Artists from './components/artists'
 import Footer from './components/footer'
 
@@ -11,9 +9,6 @@ import Footer from './components/footer'
 function App() {
 
   return (
-    <BrowserRouter basename="/bpConcept">
-      <Routes>
-        <Route path="/" element={
           <div className="relative z-0 bg-primary">
             <div className="lg:bg-hero-pattern lg:bg-cover lg:bg-no-repeat lg:bg-center overflow-x-hidden">
               <Navbar />
@@ -23,10 +18,6 @@ function App() {
               <Footer />
             </div>
           </div>
-        } />
-      </Routes>
-    </BrowserRouter>
-
   )
 }
 
